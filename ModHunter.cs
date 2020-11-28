a7a93ad73937e6be7449be84a41bde5aaeb47770
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2020-11-28 21:39:47 +0100

    472

--- ModHunter.cs
+++ ModHunter.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 
 namespace ModHunter
 {
+    public enum MessageType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
     /// <summary>
     /// ModHunter is a mod for Green Hell that allows a player to unlock all weapons, armor and traps.
 	/// A player can get tribal weapons.
@@ -15,38 +22,42 @@ namespace ModHunter
     /// </summary>
     public class ModHunter : MonoBehaviour
     {
-        private static ModHunter s_Instance;
+        private static ModHunter Instance;
 
         private static readonly string ModName = nameof(ModHunter);
+        private static readonly float ModScreenTotalWidth = 500f;
+        private static readonly float ModScreenTotalHeight = 150f;
+        private static readonly float ModScreenMinHeight = 30f;
+        private static readonly float ModScreenMaxHeight = 180f;
 
+        private static bool IsMinimized { get; set; } = false;
         private bool ShowUI = false;
 
-        public static Rect ModHunterScreen = new Rect(Screen.width / 20f, Screen.height / 20f, 450f, 150f);
+        public static Rect ModHunterScreen = new Rect(Screen.width / 6f, Screen.height / 6f, ModScreenTotalWidth, ModScreenTotalHeight);
 
-        private static ItemsManager itemsManager;
+        private static ItemsManager LocalItemsManager;
+        private static Player LocalPlayer;
+        private static HUDManager LocalHUDManager;
 
-        private static Player player;
-
-        private static HUDManager hUDManager;
-
-        private static readonly List<ItemID> m_TribalWeaponItemIDList = new List<ItemID>
+        public static List<ItemID> TribalWeaponItemIDs = new List<ItemID>
                     {
                         ItemID.metal_axe,
                         ItemID.Obsidian_Bone_Blade,
                         ItemID.Tribe_Bow,
                         ItemID.Tribe_Spear
                     };
-        private static List<ItemInfo> m_UnlockedHunterItemInfos = new List<ItemInfo>();
+        public static List<ItemInfo> UnlockedHunterItemInfos = new List<ItemInfo>();
         public bool HasUnlockedHunter { get; private set; }
         public bool InstantFinishConstructionsOption { get; private set; }
 
         public bool IsModActiveForMultiplayer { get; private set; }
         public bool IsModActiveForSingleplayer => ReplTools.AmIMaster();
 
-        public static string HUDBigInfoMessage(string message) => $"<color=#{ColorUtility.ToHtmlStringRGBA(Color.red)}>System</color>\n{message}";
-
-        public static string AddedToInventoryMessage(int count, ItemInfo itemInfo)
-            => $"<color=#{ColorUtility.ToHtmlStringRGBA(Color.green)}>Added {count} x {itemInfo.GetNameToDisplayLocalized()}</color> to inventory.";
+        public static string AlreadyUnlockedHunter() => $"All hunter items were already unlocked!";
+        public static string AddedToInventoryMessage(int count, ItemInfo itemInfo) => $"Added {count} x {itemInfo.GetNameToDisplayLocalized()} to inventory.";
+        public static string PermissionChangedMessage(string permission) => $"Permission to use mods and cheats in multiplayer was {permission}!";
+        public static string HUDBigInfoMessage(string message, MessageType messageType, Color? headcolor = null)
+            => $"<color=#{ (headcolor != null ? ColorUtility.ToHtmlStringRGBA(headcolor.Value) : ColorUtility.ToHtmlStringRGBA(Color.red))  }>{messageType}</color>\n{message}";
 
         public void Start()
         {
@@ -58,32 +69,34 @@ namespace ModHunter
             IsModActiveForMultiplayer = optionValue;
             ShowHUDBigInfo(
                           (optionValue ?
-                            HUDBigInfoMessage($"<color=#{ColorUtility.ToHtmlStringRGBA(Color.green)}>Permission to use mods for multiplayer was granted!</color>")
-                            : HUDBigInfoMessage($"<color=#{ColorUtility.ToHtmlStringRGBA(Color.yellow)}>Permission to use mods for multiplayer was revoked!</color>")),
-                           $"{ModName} Info",
-                           HUDInfoLogTextureType.Count.ToString());
+                            HUDBigInfoMessage(PermissionChangedMessage($"granted"), MessageType.Info, Color.green)
+                            : HUDBigInfoMessage(PermissionChangedMessage($"revoked"), MessageType.Info, Color.yellow))
+                            );
         }
 
         public ModHunter()
         {
             useGUILayout = true;
-            s_Instance = this;
+            Instance = this;
         }
 
         public static ModHunter Get()
         {
-            return s_Instance;
+            return Instance;
         }
 
         public void ShowHUDInfoLog(string itemID, string localizedTextKey)
         {
             Localization localization = GreenHellGame.Instance.GetLocalization();
-            ((HUDMessages)hUDManager.GetHUD(typeof(HUDMessages))).AddMessage(localization.Get(localizedTextKey) + "  " + localization.Get(itemID));
+            ((HUDMessages)LocalHUDManager.GetHUD(typeof(HUDMessages))).AddMessage(localization.Get(localizedTextKey) + "  " + localization.Get(itemID));
         }
 
-        public void ShowHUDBigInfo(string text, string header, string textureName)
+        public void ShowHUDBigInfo(string text)
         {
-            HUDBigInfo hudBigInfo = (HUDBigInfo)hUDManager.GetHUD(typeof(HUDBigInfo));
+            string header = $"{ModName} Info";
+            string textureName = HUDInfoLogTextureType.Count.ToString();
+            HUDBigInfo hudBigInfo = (HUDBigInfo)LocalHUDManager.GetHUD(typeof(HUDBigInfo));
+            HUDBigInfoData.s_Duration = 2f;
             HUDBigInfoData hudBigInfoData = new HUDBigInfoData
             {
                 m_Header = header,
@@ -98,19 +111,19 @@ namespace ModHunter
         private void EnableCursor(bool blockPlayer = false)
         {
             CursorManager.Get().ShowCursor(blockPlayer, false);
-            player = Player.Get();
+            LocalPlayer = Player.Get();
 
             if (blockPlayer)
             {
-                player.BlockMoves();
-                player.BlockRotation();
-                player.BlockInspection();
+                LocalPlayer.BlockMoves();
+                LocalPlayer.BlockRotation();
+                LocalPlayer.BlockInspection();
             }
             else
             {
-                player.UnblockMoves();
-                player.UnblockRotation();
-                player.UnblockInspection();
+                LocalPlayer.UnblockMoves();
+                LocalPlayer.UnblockRotation();
+                LocalPlayer.UnblockInspection();
             }
         }
 
@@ -154,9 +167,9 @@ namespace ModHunter
 
         private void InitData()
         {
-            hUDManager = HUDManager.Get();
-            itemsManager = ItemsManager.Get();
-            player = Player.Get();
+            LocalHUDManager = HUDManager.Get();
+            LocalItemsManager = ItemsManager.Get();
+            LocalPlayer = Player.Get();
         }
 
         private void InitSkinUI()
@@ -168,39 +181,79 @@ namespace ModHunter
         {
             using (var verticalScope = new GUILayout.VerticalScope(GUI.skin.box))
             {
-                if (GUI.Button(new Rect(430f, 0f, 20f, 20f), "X", GUI.skin.button))
+                ScreenMenuBox();
+                if (!IsMinimized)
                 {
-                    CloseWindow();
-                }
-
-                using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
-                {
-                    GUILayout.Label("Weapon-, armor- and trap blueprints", GUI.skin.label,  GUILayout.MaxWidth(200f));
-                    if (GUILayout.Button("Unlock hunter", GUI.skin.button))
-                    {
-                        OnClickUnlockHunterButton();
-                    }
-                }
-
-                using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
-                {
-                    GUILayout.Label("Metal axe, bow, spear and obsidian bone blade", GUI.skin.label, GUILayout.MaxWidth(200f));
-                    if (GUILayout.Button("Get tribal weapons", GUI.skin.button))
-                    {
-                        OnClickGetTribalWeaponsButton();
-                    }
-                }
-
-                using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
-                {
-                    GUILayout.Label("Add 5 tribal arrows to the backpack", GUI.skin.label, GUILayout.MaxWidth(200f));
-                    if (GUILayout.Button("5 x tribal arrows", GUI.skin.button))
-                    {
-                        OnClickGetTribalArrowsButton();
-                    }
+                    UnlockHunterBox();
+                    GetTribalWeaponsBox();
+                    GetTribalArrowsBox();
                 }
             }
             GUI.DragWindow(new Rect(0f, 0f, 10000f, 10000f));
+        }
+
+        private void ScreenMenuBox()
+        {
+            if (GUI.Button(new Rect(ModHunterScreen.width - 40f, 0f, 20f, 20f), "-", GUI.skin.button))
+            {
+                CollapseWindow();
+            }
+
+            if (GUI.Button(new Rect(ModHunterScreen.width - 20f, 0f, 20f, 20f), "X", GUI.skin.button))
+            {
+                CloseWindow();
+            }
+        }
+
+        private void CollapseWindow()
+        {
+            if (!IsMinimized)
+            {
+                ModHunterScreen.Set(ModHunterScreen.x, Screen.height - ModScreenMinHeight, ModScreenTotalWidth, ModScreenMinHeight);
+                IsMinimized = true;
+            }
+            else
+            {
+                ModHunterScreen.Set(ModHunterScreen.x, Screen.height / ModScreenMinHeight, ModScreenTotalWidth, ModScreenTotalHeight);
+                IsMinimized = false;
+            }
+            InitWindow();
+        }
+
+        private void GetTribalArrowsBox()
+        {
+            using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
+            {
+                GUILayout.Label("Add 5 tribal arrows to the backpack", GUI.skin.label, GUILayout.MaxWidth(200f));
+                if (GUILayout.Button("5 x tribal arrows", GUI.skin.button))
+                {
+                    OnClickGetTribalArrowsButton();
+                }
+            }
+        }
+
+        private void GetTribalWeaponsBox()
+        {
+            using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
+            {
+                GUILayout.Label("Metal axe, bow, spear and obsidian bone blade", GUI.skin.label, GUILayout.MaxWidth(200f));
+                if (GUILayout.Button("Get tribal weapons", GUI.skin.button))
+                {
+                    OnClickGetTribalWeaponsButton();
+                }
+            }
+        }
+
+        private void UnlockHunterBox()
+        {
+            using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
+            {
+                GUILayout.Label("Weapon-, armor- and trap blueprints", GUI.skin.label, GUILayout.MaxWidth(200f));
+                if (GUILayout.Button("Unlock hunter", GUI.skin.button))
+                {
+                    OnClickUnlockHunterButton();
+                }
+            }
         }
 
         private void CloseWindow()
@@ -251,33 +304,29 @@ namespace ModHunter
             {
                 if (!HasUnlockedHunter)
                 {
-                    m_UnlockedHunterItemInfos = itemsManager.GetAllInfos().Values.Where(info => info.IsWeapon() || info.IsArmor() || ItemInfo.IsTrap(info.m_ID)).ToList();
+                    UnlockedHunterItemInfos = LocalItemsManager.GetAllInfos().Values.Where(info => info.IsWeapon() || info.IsArmor() || ItemInfo.IsTrap(info.m_ID)).ToList();
 
-                    if (!m_UnlockedHunterItemInfos.Contains(itemsManager.GetInfo(ItemID.Frog_Stretcher)))
+                    if (!UnlockedHunterItemInfos.Contains(LocalItemsManager.GetInfo(ItemID.Frog_Stretcher)))
                     {
-                        m_UnlockedHunterItemInfos.Add(itemsManager.GetInfo(ItemID.Frog_Stretcher));
+                        UnlockedHunterItemInfos.Add(LocalItemsManager.GetInfo(ItemID.Frog_Stretcher));
                     }
 
-                    foreach (ItemInfo hunterItemInfo in m_UnlockedHunterItemInfos)
+                    foreach (ItemInfo hunterItemInfo in UnlockedHunterItemInfos)
                     {
-                        itemsManager.UnlockItemInNotepad(hunterItemInfo.m_ID);
-                        itemsManager.UnlockItemInfo(hunterItemInfo.m_ID.ToString());
+                        LocalItemsManager.UnlockItemInNotepad(hunterItemInfo.m_ID);
+                        LocalItemsManager.UnlockItemInfo(hunterItemInfo.m_ID.ToString());
                         ShowHUDInfoLog(hunterItemInfo.m_ID.ToString(), "HUD_InfoLog_NewEntry");
                     }
                     HasUnlockedHunter = true;
                 }
                 else
                 {
-                    ShowHUDBigInfo(
-                        HUDBigInfoMessage(
-                            $"<color=#{ColorUtility.ToHtmlStringRGBA(Color.yellow)}>All hunter items were already unlocked!</color>"),
-                        $"{ModName} Info",
-                        HUDInfoLogTextureType.Count.ToString());
+                    ShowHUDBigInfo(HUDBigInfoMessage(AlreadyUnlockedHunter(), MessageType.Warning, Color.yellow));
                 }
             }
             catch (Exception exc)
             {
-                ModAPI.Log.Write($"[{ModName}.{ModName}:{nameof(UnlockHunter)}] throws exception:\n{exc.Message}");
+                ModAPI.Log.Write($"[{ModName}:{nameof(UnlockHunter)}] throws exception:\n{exc.Message}");
             }
         }
 
@@ -285,20 +334,17 @@ namespace ModHunter
         {
             try
             {
-                foreach (ItemID tribalWeaponItemID in m_TribalWeaponItemIDList)
+                foreach (ItemID tribalWeaponItemID in TribalWeaponItemIDs)
                 {
-                    ItemInfo tribalWeaponItemInfo = itemsManager.GetInfo(tribalWeaponItemID);
-                    itemsManager.UnlockItemInfo(tribalWeaponItemInfo.m_ID.ToString());
-                    player.AddItemToInventory(tribalWeaponItemInfo.m_ID.ToString());
-                    ShowHUDBigInfo(
-                         HUDBigInfoMessage(AddedToInventoryMessage(1, tribalWeaponItemInfo)),
-                         $"{ModName} Info",
-                         HUDInfoLogTextureType.Count.ToString());
+                    ItemInfo tribalWeaponItemInfo = LocalItemsManager.GetInfo(tribalWeaponItemID);
+                    LocalItemsManager.UnlockItemInfo(tribalWeaponItemInfo.m_ID.ToString());
+                    LocalPlayer.AddItemToInventory(tribalWeaponItemInfo.m_ID.ToString());
+                    ShowHUDBigInfo(HUDBigInfoMessage(AddedToInventoryMessage(1, tribalWeaponItemInfo), MessageType.Info, Color.green));
                 }
             }
             catch (Exception exc)
             {
-                ModAPI.Log.Write($"[{ModName}.{ModName}:{nameof(GetTribalMeleeWeapons)}] throws exception:\n{exc.Message}");
+                ModAPI.Log.Write($"[{ModName}:{nameof(GetTribalMeleeWeapons)}] throws exception:\n{exc.Message}");
             }
         }
 
@@ -310,20 +356,17 @@ namespace ModHunter
                 {
                     count = 5;
                 }
-                itemsManager.UnlockItemInfo(ItemID.Tribe_Arrow.ToString());
-                ItemInfo tribalArrowItemInfo = itemsManager.GetInfo(ItemID.Tribe_Arrow);
+                LocalItemsManager.UnlockItemInfo(ItemID.Tribe_Arrow.ToString());
+                ItemInfo tribalArrowItemInfo = LocalItemsManager.GetInfo(ItemID.Tribe_Arrow);
                 for (int i = 0; i < count; i++)
                 {
-                    player.AddItemToInventory(tribalArrowItemInfo.m_ID.ToString());
+                    LocalPlayer.AddItemToInventory(tribalArrowItemInfo.m_ID.ToString());
                 }
-                ShowHUDBigInfo(
-                         HUDBigInfoMessage(AddedToInventoryMessage(count, tribalArrowItemInfo)),
-                         $"{ModName} Info",
-                         HUDInfoLogTextureType.Count.ToString());
+                ShowHUDBigInfo(HUDBigInfoMessage(AddedToInventoryMessage(count, tribalArrowItemInfo), MessageType.Info, Color.green));
             }
             catch (Exception exc)
             {
-                ModAPI.Log.Write($"[{ModName}.{ModName}:{nameof(GetMaxFiveTribalArrows)}] throws exception:\n{exc.Message}");
+                ModAPI.Log.Write($"[{ModName}:{nameof(GetMaxFiveTribalArrows)}] throws exception:\n{exc.Message}");
             }
         }
     }
